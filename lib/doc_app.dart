80d3009a6de7d218a38b486aac903e6a0a820a34
Abdurrahman.Alboghdady@gmail.com <ibrahim.elbaz.1011@gmail.com>
2024-09-05 19:51:59 +0300

    clean app to start

---
+++ lib/doc_app.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class DocApp extends StatelessWidget {
+  const DocApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Container(),
+    );
+  }
+}
